d8ae2a218f0e212f31b3392cf18803c331db22d4
Peter deHaan <peter@deseloper.com>
2014-03-22 22:49:21 -0700

    adding shrinkwrap validator

---
+++ index.js
@@ -0,0 +1,6 @@
+var test = require('./lib/test');
+
+console.log('well, this was disappointing.');
+
+console.log('- %s', test.foo());
+console.log('- %s', test.bar());
---
+++ tasks/validate-shrinkwrap.js
@@ -0,0 +1,8 @@
+module.exports = function (grunt) {
+  'use strict';
+
+  grunt.loadNpmTasks('grunt-nsp-shrinkwrap');
+
+  grunt.config('validate-shrinkwrap', {
+  });
+};
